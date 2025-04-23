49606cda1a76a07197c8ae53f2bfa28f1d8e4e2f
Jari Petays <jari.petays@iki.fi>
2025-04-23 11:09:16 +0300

    add ZLogger for testing

--- Assets/Scripts/Demo/DemoLoader.cs
+++ Assets/Scripts/Demo/DemoLoader.cs
@@ -1,61 +1,95 @@
 using System.Collections;
+using System.IO;
 using System.Text;
 using Demo.Audio;
+using Microsoft.Extensions.Logging;
 using Prg;
 using Prg.Util;
 using UnityEngine;
-using Debug = Prg.Debug;
+using ZLogger;
+using ZLogger.Unity;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Demo
 {
-    internal class DemoLoader : MonoBehaviour
+    public static class MyLoggerFactory
     {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void SubsystemRegistration()
-        {
-            // Manual reset if UNITY Domain Reloading is disabled.
-            Debug.Log("SubsystemRegistration");
-            LogConfig.Create();
-        }
+        private static ILoggerFactory? _loggerFactory;
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        private static void BeforeSceneLoad()
+        public static ILogger CreateLogger<T>()
         {
-            Debug.Log("BeforeSceneLoad");
-            var parent = new GameObject(nameof(DemoLoader));
-            var loader = parent.AddComponent<DemoLoader>();
-            // Start async services ASAP.
-            loader.StartCoroutine(LoadServicesAsync());
-        }
-
-        private void Awake()
-        {
-            var startupMessage = new StringBuilder()
-                .Append(" Game ").Append(Application.productName)
-                .Append(" Ver ").Append(Application.version)
-                .Append(" Plat ").Append(AppPlatform.IsSimulator ? "Simulator" : AppPlatform.Name)
-                .Append(" Screen ").Append(AppPlatform.Resolution())
-                .ToString();
-            Debug.Log(startupMessage);
-        }
-
-        private void Start()
-        {
-            Debug.Log($"Start() {RichText.Yellow($"frame #{Time.frameCount}")} start");
-            // UNITY Audio Mixer needs to be started in Start()!
-            AudioConfig.Initialize();
-            Debug.Log($"Start() {RichText.Yellow($"frame #{Time.frameCount}")} done");
-        }
-
-        private static IEnumerator LoadServicesAsync()
-        {
-            Debug.Log($"LoadServicesAsync {RichText.Yellow($"frame #{Time.frameCount}")} start");
-            yield return null;
-            if (AppPlatform.IsMobile)
+            _loggerFactory ??= LoggerFactory.Create(logging =>
             {
-                MobileAudio.Initialize();
-            }
-            Debug.Log($"LoadServicesAsync {RichText.Yellow($"frame #{Time.frameCount}")} done");
+                logging.SetMinimumLevel(LogLevel.Trace);
+                // log to UnityDebug
+                logging.AddZLoggerUnityDebug();
+                // Log to file.
+                logging.AddZLoggerFile(Path.Combine(Application.persistentDataPath, LogFilename()));
+            });
+            return _loggerFactory.CreateLogger<T>();
         }
+
+        private static string LogFilename()
+        {
+            var prefix = AppPlatform.IsEditor
+                ? "editor"
+                : Application.platform.ToString().ToLower().Replace("player", string.Empty);
+            var baseName = Application.productName.Replace(" ", "_").ToLower();
+            return $"{prefix}_{baseName}_z.log";
+        }
+    }
+}
+
+internal class DemoLoader : MonoBehaviour
+{
+    private static readonly ILogger Logger = Demo.MyLoggerFactory.CreateLogger<DemoLoader>();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    private static void SubsystemRegistration()
+    {
+        // Manual reset if UNITY Domain Reloading is disabled.
+        Logger.ZLogTrace($"SubsystemRegistration");
+        LogConfig.Create();
+        Logger.ZLogInformation($"Starting {nameof(DemoLoader)}");
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void BeforeSceneLoad()
+    {
+        Logger.ZLogTrace($"BeforeSceneLoad");
+        var parent = new GameObject(nameof(DemoLoader));
+        var loader = parent.AddComponent<DemoLoader>();
+        // Start async services ASAP.
+        loader.StartCoroutine(LoadServicesAsync());
+    }
+
+    private void Awake()
+    {
+        var startupMessage = new StringBuilder()
+            .Append(" Game ").Append(Application.productName)
+            .Append(" Ver ").Append(Application.version)
+            .Append(" Plat ").Append(AppPlatform.IsSimulator ? "Simulator" : AppPlatform.Name)
+            .Append(" Screen ").Append(AppPlatform.Resolution())
+            .ToString();
+        Logger.ZLogTrace($"{startupMessage}");
+    }
+
+    private void Start()
+    {
+        Logger.ZLogTrace($"Start() {RichText.Yellow($"frame #{Time.frameCount}")} start");
+        // UNITY Audio Mixer needs to be started in Start()!
+        AudioConfig.Initialize();
+        Logger.ZLogTrace($"Start() {RichText.Yellow($"frame #{Time.frameCount}")} done");
+    }
+
+    private static IEnumerator LoadServicesAsync()
+    {
+        Logger.ZLogTrace($"LoadServicesAsync {RichText.Yellow($"frame #{Time.frameCount}")} start");
+        yield return null;
+        if (AppPlatform.IsMobile)
+        {
+            MobileAudio.Initialize();
+        }
+        Logger.ZLogTrace($"LoadServicesAsync {RichText.Yellow($"frame #{Time.frameCount}")} done");
     }
 }
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Project external dependencies
+
+# NuGetForUnity
+
+[NuGetForUnity](https://github.com/GlitchEnzo/NuGetForUnity)
+
+# CsprojModifier
+
+[CsprojModifier](https://github.com/Cysharp/CsprojModifier) for 'latest' C# 10 language options (nullable)
+
+# ZLogger
+
+[ZLogger](https://github.com/Cysharp/ZLogger)
+
+[UNITY installation](https://github.com/Cysharp/ZLogger?tab=readme-ov-file#unity)
